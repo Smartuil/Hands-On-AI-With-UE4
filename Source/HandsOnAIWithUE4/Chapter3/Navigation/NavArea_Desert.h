0b1fa29ec412e817aeaa07105b9d7ebb5116b94a
Smartuil <563412673@qq.com>
2020-06-08 17:52:51 +0800

    第三章上

---
+++ Source/HandsOnAIWithUE4/Chapter3/Navigation/NavArea_Desert.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NavAreas/NavArea.h"
+#include "NavArea_Desert.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HANDSONAIWITHUE4_API UNavArea_Desert : public UNavArea
+{
+	GENERATED_BODY()
+
+	UNavArea_Desert();
+};
